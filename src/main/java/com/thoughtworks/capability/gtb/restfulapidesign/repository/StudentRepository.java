9377ad4e454578cf31dc8d7caf6f5b4beb877ea8
liyuan <823260355@qq.com>
2020-11-11 20:55:06 +0800

    [ly] finish register student

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.repository;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
+
+import java.util.ArrayList;
+
+public class StudentRepository {
+    private ArrayList<Student> students = new ArrayList<>();
+}
---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.service;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
+import com.thoughtworks.capability.gtb.restfulapidesign.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudents(Student student) {
+        students.add(student);
+    }
+}
